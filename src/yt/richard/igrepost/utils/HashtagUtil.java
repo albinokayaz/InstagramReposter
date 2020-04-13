62ee33cd2bca2ab3e233283a10e5aabadd79559f
Richard <mail@richard.yt>
2020-04-13 10:23:19 +0200

    Dynamic hashtags were not that good so reverted that
    Signed-off-by: Richard <mail@richard.yt>

--- src/yt/richard/igrepost/utils/HashtagUtil.java
+++ src/yt/richard/igrepost/utils/HashtagUtil.java
@@ -3,20 +3,25 @@ package yt.richard.igrepost.utils;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import org.json.JSONArray;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class HashtagUtil {
 
-  public static List<String> getHashtags() throws IOException {
+  public static List<String> getDynamicHashtags() throws IOException {
     List<String> hashtags = new ArrayList<>();
     Document doc = Jsoup.connect("https://app.sistrix.com/app_instagram/_result/lang/de?tag=meme").get();
-    Elements tagsHTML = doc.getElementsByClass("instatag instatext");
-    for (Element instatag : tagsHTML) {
-      hashtags.add(instatag.html());
-    }
+    Elements tagsList = doc.getElementsByClass("instatag instatext");
+    tagsList.forEach(tag -> hashtags.add(tag.html()));
+    return hashtags;
+  }
+
+  public static List<String> getHashtags() {
+    List<String> hashtags = new ArrayList<>();
+    JSONArray tagsList = JsonUtil.retrieveJsonArray("https://richard.yt/instagram/hashtags.json");
+    tagsList.forEach(tag -> hashtags.add((String) tag));
     return hashtags;
   }
 
